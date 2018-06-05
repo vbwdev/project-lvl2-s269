9835d317077e855605103e7ed3bdee85bb581f6f
vbwdev <vbwdev@gmail.com>
2018-06-05 20:28:53 +0300

    Add genDiff tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,5 +1,24 @@
-// import genDiff from '../src';
+import genDiff, { parseConfigs, buildDiff } from '../src';
 
-test('should work', () => {
-  expect(1 + 2).toBe(3);
+import firstConfig from './__fixtures__/before.json';
+import secondConfig from './__fixtures__/after.json';
+import parsedConfigs from './__fixtures__/parsed';
+import diffResult from './__fixtures__/diffResult';
+
+describe('genDiff tests', () => {
+  test('should work', () => {
+    expect(genDiff(firstConfig, secondConfig)).toBe(diffResult);
+  });
+
+  test('config parsing', () => {
+    expect(parseConfigs(
+      { ...firstConfig, ...secondConfig },
+      firstConfig,
+      secondConfig,
+    )).toEqual(parsedConfigs);
+  });
+
+  test('diff building', () => {
+    expect(buildDiff(parsedConfigs)).toBe(diffResult);
+  });
 });
