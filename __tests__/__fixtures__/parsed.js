4a0e57646d2a1b957a64ea6d1b4823cab0de49c9
vbwdev <vbwdev@gmail.com>
2018-06-05 20:25:42 +0300

    Add fixtures for tests

---
+++ __tests__/__fixtures__/parsed.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    key: 'host',
+    firstConfigValue: 'hexlet.io',
+    secondConfigValue: 'hexlet.io',
+    isSame: true,
+  },
+  {
+    key: 'timeout',
+    firstConfigValue: 50,
+    secondConfigValue: 20,
+    isSame: false,
+  },
+  {
+    key: 'proxy',
+    firstConfigValue: '123.234.53.22',
+    secondConfigValue: undefined,
+    isSame: false,
+  },
+  {
+    key: 'verbose',
+    firstConfigValue: undefined,
+    secondConfigValue: true,
+    isSame: false,
+  },
+]
