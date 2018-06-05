f06e5c2bff432b77eaf438dc63f5344fb2b63a24
vbwdev <vbwdev@gmail.com>
2018-06-05 20:28:24 +0300

    Add genDiff functionality to bin file

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import fs from 'fs';
+
+import genDiff from '..';
 
 program
   .version('0.0.1')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
+  .action((firstConfig, secondConfig) => {
+    const firstConfigContent = JSON.parse(fs.readFileSync(firstConfig));
+    const secondConfigContent = JSON.parse(fs.readFileSync(secondConfig));
+    const configsDiff = genDiff(firstConfigContent, secondConfigContent);
+    console.log(configsDiff);
+  })
   .parse(process.argv);
