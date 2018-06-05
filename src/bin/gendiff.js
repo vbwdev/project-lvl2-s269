851a18d3567d0b4ff0e18f6af4fd0045aef58c5d
vbwdev <vbwdev@gmail.com>
2018-06-06 01:48:03 +0300

    Fix version in bin file

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,7 +6,7 @@ import fs from 'fs';
 import genDiff from '..';
 
 program
-  .version('0.0.1')
+  .version('0.1.0')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
