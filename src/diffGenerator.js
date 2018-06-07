72286fbaa9b179b4d491c703a63f47563b3a015c
vbwdev <vbwdev@gmail.com>
2018-06-07 23:42:58 +0300

    Fix diff generator problem with falsy data

--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -1,49 +1,52 @@
 import _ from 'lodash';
 import { STATUS } from './constants';
 
-const prepareDiff = (key, firstValue, secondValue) => {
-  if (_.isPlainObject(firstValue) && _.isPlainObject(secondValue)) {
-    return {
-      key,
-      status: STATUS.NOT_CHANGED,
-      children: generateDiff(firstValue, secondValue),
-    };
-  }
-
-  if (firstValue && secondValue && firstValue === secondValue) {
-    return {
-      key,
-      status: STATUS.NOT_CHANGED,
-      value: firstValue,
-    };
-  }
-
-  if (firstValue && secondValue && firstValue !== secondValue) {
-    return {
-      key,
-      status: STATUS.CHANGED,
-      value: [firstValue, secondValue],
-    };
-  }
-
-  if (firstValue) {
-    return {
-      key,
-      status: STATUS.DELETED,
-      value: firstValue,
-    };
-  }
-
-  if (secondValue) {
-    return {
-      key,
-      status: STATUS.ADDED,
-      value: secondValue,
-    };
-  }
-};
-
 const generateDiff = (firstContent, secondContent) => {
+  const prepareDiff = (key, firstValue, secondValue) => {
+    const hasFirstValue = _.has(firstContent, key);
+    const hasSecondValue = _.has(secondContent, key);
+
+    if (_.isPlainObject(firstValue) && _.isPlainObject(secondValue)) {
+      return {
+        key,
+        status: STATUS.NOT_CHANGED,
+        children: generateDiff(firstValue, secondValue),
+      };
+    }
+
+    if (hasFirstValue && hasSecondValue && firstValue === secondValue) {
+      return {
+        key,
+        status: STATUS.NOT_CHANGED,
+        value: firstValue,
+      };
+    }
+
+    if (hasFirstValue && hasSecondValue && firstValue !== secondValue) {
+      return {
+        key,
+        status: STATUS.CHANGED,
+        value: [firstValue, secondValue],
+      };
+    }
+
+    if (hasFirstValue) {
+      return {
+        key,
+        status: STATUS.DELETED,
+        value: firstValue,
+      };
+    }
+
+    if (hasSecondValue) {
+      return {
+        key,
+        status: STATUS.ADDED,
+        value: secondValue,
+      };
+    }
+  };
+
   const keys = _.union(_.keys(firstContent), _.keys(secondContent));
   return keys.reduce(
     (acc, key) => [...acc, prepareDiff(key, firstContent[key], secondContent[key])],
