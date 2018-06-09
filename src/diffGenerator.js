add8f154a9fcb560c99aabc44c12b5f4c8399975
vbwdev <vbwdev@gmail.com>
2018-06-09 13:06:03 +0300

    Rework diffGenerator with dynamic dispatching by type

--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -1,71 +1,46 @@
 /* eslint-disable consistent-return */
 import _ from 'lodash';
 
+const statuses = [
+  {
+    status: 'nested',
+    check: (first, second, key) =>
+      _.has(first, key) && _.has(second, key) && _.isPlainObject(first[key]) && _.isPlainObject(second[key]),
+  },
+  {
+    status: 'unchanged',
+    check: (first, second, key) => _.has(first, key) && _.has(second, key) && first[key] === second[key],
+  },
+  {
+    status: 'changed',
+    check: (first, second, key) => _.has(first, key) && _.has(second, key) && first[key] !== second[key],
+  },
+  {
+    status: 'deleted',
+    check: (first, second, key) => _.has(first, key) && !_.has(second, key),
+  },
+  {
+    status: 'added',
+    check: (first, second, key) => !_.has(first, key) && _.has(second, key),
+  },
+];
+
 const generateDiff = (firstContent, secondContent) => {
   if (!_.isPlainObject(firstContent) || !_.isPlainObject(secondContent)) {
     return [];
   }
 
-  const prepareDiff = (key, firstValue, secondValue) => {
-    const hasFirstValue = _.has(firstContent, key);
-    const hasSecondValue = _.has(secondContent, key);
-
-    if (_.isPlainObject(firstValue) && _.isPlainObject(secondValue)) {
-      return {
-        key,
-        status: 'nested',
-        oldValue: firstValue,
-        newValue: secondValue,
-        children: generateDiff(firstValue, secondValue),
-      };
-    }
-
-    if (hasFirstValue && hasSecondValue && firstValue === secondValue) {
-      return {
-        key,
-        status: 'unchanged',
-        oldValue: firstValue,
-        newValue: secondValue,
-        children: generateDiff(firstValue, secondValue),
-      };
-    }
-
-    if (hasFirstValue && hasSecondValue && firstValue !== secondValue) {
-      return {
-        key,
-        status: 'changed',
-        oldValue: firstValue,
-        newValue: secondValue,
-        children: generateDiff(firstValue, secondValue),
-      };
-    }
-
-    if (hasFirstValue && !hasSecondValue) {
-      return {
-        key,
-        status: 'deleted',
-        oldValue: firstValue,
-        newValue: secondValue,
-        children: generateDiff(firstValue, secondValue),
-      };
-    }
-
-    if (!hasFirstValue && hasSecondValue) {
-      return {
-        key,
-        status: 'added',
-        oldValue: firstValue,
-        newValue: secondValue,
-        children: generateDiff(firstValue, secondValue),
-      };
-    }
-  };
-
   const keys = _.union(_.keys(firstContent), _.keys(secondContent));
-  return keys.reduce(
-    (acc, key) => [...acc, prepareDiff(key, firstContent[key], secondContent[key])],
-    [],
-  );
+  return keys.reduce((acc, key) => {
+    const { status } = _.find(statuses, ({ check }) => check(firstContent, secondContent, key));
+    return [...acc, {
+      key,
+      status,
+      oldValue: firstContent[key],
+      newValue: secondContent[key],
+      children: generateDiff(firstContent[key], secondContent[key]),
+    }];
+  }, []);
 };
 
 export default generateDiff;
