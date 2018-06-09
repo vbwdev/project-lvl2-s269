c07ff1241b5c61d5b523492fc43868696a9fd866
vbwdev <vbwdev@gmail.com>
2018-06-10 00:50:08 +0300

    Fix renderer imports

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,13 +1,9 @@
-import renderDiffOriginal from './original';
-import renderDiffPlain from './plain';
-import renderDiffJson from './json';
+import original from './original';
+import plain from './plain';
+import json from './json';
 
-const renderers = {
-  original: renderDiffOriginal,
-  plain: renderDiffPlain,
-  json: renderDiffJson,
+export {
+  original,
+  plain,
+  json,
 };
-
-const render = (format, diff) => renderers[format](diff);
-
-export default render;
